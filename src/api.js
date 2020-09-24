befdb12aa42234ff191b58bda6efc0acd926d1a1
victor  gabriel <victorgabrieljp8@gmail.com>
2020-09-24 22:51:49 +0000

    Criação API

--- src/api.js
+++ src/api.js
@@ -1,19 +1,61 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
+
+const app = express();
 app.use(cors());
 app.use(express.json());
-const port = 3000;
-const txt_user = [];
-let key = 0;
 
-app.post('/enviar',(req,res) =>{
-    const {txt} = req.body;
-    key ++;
-    const user = {key,txt}
-    txt_user.push(user);
-    res.status(201).json(user); 
+const port = 3000;
+const txts = [];
+//let key = 0;
+app.get ("/txt", (req, res) => {
+  let nomes = []
+  for (let i = 0; i < txts.lenght; i++) {
+    nomes.append = txts[i];
+  }
+  res.json(nomes);
 })
-app.listen(port,()=>{
-    console.log("app is running is port 3000")
-})
+
+/*
+[
+  {
+    nome:"ae",
+    txt:"opa"
+  },
+
+  {
+    nome:"ae2"
+    txt:"eae men"
+  }
+]
+*/
+
+
+// Envia conteúdo do arquivo requisitado
+app.get("/atualizar/:nome", (req, res) => {
+  const nome = req.params.nome;
+
+  for (let i = 0; i < txts.lenght; i++){
+    arquivo = txts[i];
+    if (arquivo.nome == nome){
+      res.json(arquivo);
+    } 
+  }
+
+  res.status(404).json({});
+});
+
+// Recebe txt novo a ser adicionado
+app.post('/enviar', (req,res) => {
+  const arquivo = req.body;
+  //key ++;
+  //const user = {key,txt}
+  txts.push(arquivo);
+  
+  res.status(201).json(arquivo); 
+});
+
+app.listen(port, () => {
+  console.log(`app is running is port ${port}`)
+});
+
